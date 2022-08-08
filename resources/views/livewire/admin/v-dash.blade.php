0565b020626b4592bf9a9160e54ed9124c39a1ba
jnn95 <nlandungunda@gmail.com>
2022-08-09 00:04:56 +0200

    Mon premier commit

--- resources/views/livewire/admin/v-dash.blade.php
+++ resources/views/livewire/admin/v-dash.blade.php
@@ -1,3 +1,213 @@
-<div>
-    {{-- If your happiness depends on money, you will never be happy with yourself. --}}
+<div class="mx-auto bg-grey-400">
+    <div class="min-h-screen flex flex-col">
+        <!--/En-tête-->
+
+        <div class="flex flex-1">
+            <!--Principal-->
+            <main class="bg-white-300 flex-1 p-3 overflow-hidden">
+                <div class="flex flex-col">
+                    <!-- La ligne de statistiques commence ici -->
+                    <div class="flex flex-1 flex-col md:flex-row lg:flex-row mx-2">
+                        <div class="shadow-lg bg-red-700 border-l-8 hover:bg-red-500 border-black rounded rounded-t-lg mb-2 p-2 md:w-1/4 mx-2">
+                            <div class="p-4 flex flex-col">
+                                <a href="#" class="no-underline text-white text-2xl">
+                                    244
+                                </a>
+                                <a href="#" class="no-underline text-white text-lg">
+                                    Total Sales
+                                </a>
+                            </div>
+                        </div>
+                        <div class="shadow bg-green-700 border-l-8 hover:bg-green-500 border-black rounded rounded-t-lg mb-2 p-2 md:w-1/4 mx-2">
+                            <div class="p-4 flex flex-col">
+                                <a href="#" class="no-underline text-white text-2xl">
+                                    199
+                                </a>
+                                <a href="#" class="no-underline text-white text-lg">
+                                    Total Cost
+                                </a>
+                            </div>
+                        </div>
+                        <div class="shadow bg-yellow-700 border-l-8 hover:bg-yellow-500 border-black rounded rounded-t-lg mb-2 p-2 md:w-1/4 mx-2">
+                            <div class="p-4 flex flex-col">
+                                <a href="#" class="no-underline text-white text-2xl">
+                                    900
+                                </a>
+                                <a href="#" class="no-underline text-white text-lg">
+                                    Total Users
+                                </a>
+                            </div>
+                        </div>
+                        <div class="shadow bg-sky-700 border-l-8 hover:bg-sky-500 border-black rounded rounded-t-lg mb-2 p-2 md:w-1/4 mx-2">
+                            <div class="p-4 flex flex-col">
+                                <a href="#" class="no-underline text-white text-2xl">
+                                    500
+                                </a>
+                                <a href="#" class="no-underline text-white text-lg">
+                                    Total Products
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- /La ligne de statistiques se termine ici -->
+
+                    <!-- La sextion des cartes commence ici -->
+                    <div class="flex flex-1 flex-col md:flex-row lg:flex-row mx-2 bg-black">
+                        <!-- card -->
+                        <div class="rounded overflow-hidden shadow bg-white mx-2 w-full">
+                            <div class="px-6 py-2 border-b border-light-grey">
+                                <div class="font-bold text-xl">Derniers clients</div>
+                            </div>
+                            <div class="px-4 py-4 table-responsive">
+                                <table class="w-full md:w-auto">
+                                    <thead class="border-2 bg-red-700 text-white">
+                                        <tr>
+                                            <th>Entête 1</th>
+                                            <th>Entête 2</th>
+                                            <th>Entête 3</th>
+                                            <th>Entête 4</th>
+                                            <th>Entête 5</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="border-2">
+                                            <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                                            <td>Malcolm Lockyer</td>
+                                            <td>1961</td>
+                                            <td>1961</td>
+                                            <td>1961</td>
+                                        </tr>
+                                        <tr class="border-2">
+                                            <td>Witchy Woman</td>
+                                            <td>The Eagles</td>
+                                            <td>1972</td>
+                                            <td>1961</td>
+                                            <td>1961</td>
+                                        </tr>
+                                        <tr class="border-2">
+                                            <td>Shining Star</td>
+                                            <td>Earth, Wind, and Fire</td>
+                                            <td>1975</td>
+                                            <td>1961</td>
+                                            <td>1961</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                        <!-- /card -->
+                    </div>
+                    <!-- /La section des cartes se termine ici -->
+
+                    <!-- Barre de progression -->
+                    <div class="flex flex-1 flex-col md:flex-row lg:flex-row mx-2 mt-2">
+                        <div class="rounded overflow-hidden shadow bg-white mx-2 w-full pt-2">
+                            <div class="px-6 py-2 border-b border-light-grey">
+                                <div class="font-bold text-xl">Progress Among Projects</div>
+                            </div>
+                            <div class="">
+                                <div class="w-full">
+                                    <div class="shadow w-full bg-grey-light">
+                                        <div class="bg-blue-500 text-xs leading-none py-1 text-center text-white"
+                                             style="width: 45%">45%
+                                        </div>
+                                    </div>
+                                    <div class="shadow w-full bg-grey-light mt-2">
+                                        <div class="bg-teal-500 text-xs leading-none py-1 text-center text-white"
+                                             style="width: 55%">55%
+                                        </div>
+                                    </div>
+                                    <div class="shadow w-full bg-grey-light mt-2">
+                                        <div class="bg-orange-500 text-xs leading-none py-1 text-center text-white"
+                                             style="width: 65%">65%
+                                        </div>
+                                    </div>
+                                    <div class="shadow w-full bg-grey-300 mt-2">
+                                        <div class="bg-red-800 text-xs leading-none py-1 text-center text-white"
+                                             style="width: 75%">75%
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--Onglets de profil-->
+                    <div class="flex flex-1 flex-col md:flex-row lg:flex-row mx-2 p-1 mt-2 mx-auto lg:mx-2 md:mx-2 justify-between">
+                        <!--Top user 1-->
+                        <div class="rounded rounded-t-lg overflow-hidden shadow max-w-xs my-3">
+                            <img src="https://therichpost.com/wp-content/uploads/2021/04/flip-3.jpg" alt="" class="w-full"/>
+                            <div class="flex justify-center -mt-8">
+                                <img src="https://therichpost.com/wp-content/uploads/2021/03/avatar8.png" alt=""
+                                     class="responsvie-img rounded-full border-solid border-white border-2 -mt-3">
+                            </div>
+                            <div class="text-center px-3 pb-6 pt-2">
+                                <h3 class="text-black text-sm bold font-sans">Olivia Dunham</h3>
+                                <p class="mt-2 font-sans font-light text-grey-700">Hello, i'm from another the other
+                                    side!</p>
+                            </div>
+                            <div class="flex justify-center pb-3 text-grey-dark">
+                                <div class="text-center mr-3 border-r pr-3">
+                                    <h2>34</h2>
+                                    <span>Photos</span>
+                                </div>
+                                <div class="text-center">
+                                    <h2>42</h2>
+                                    <span>Friends</span>
+                                </div>
+                            </div>
+                        </div>
+                        <!--Top user 2-->
+                        <div class="rounded rounded-t-lg overflow-hidden shadow max-w-xs my-3">
+                            <img src="https://therichpost.com/wp-content/uploads/2021/04/flip-3.jpg" alt="" class="w-full"/>
+                            <div class="flex justify-center -mt-8">
+                                <img src="https://therichpost.com/wp-content/uploads/2021/03/avatar8.png" alt=""
+                                     class="responsvie-img rounded-full border-solid border-white border-2 -mt-3">
+                            </div>
+                            <div class="text-center px-3 pb-6 pt-2">
+                                <h3 class="text-black text-sm bold font-sans">Olivia Dunham</h3>
+                                <p class="mt-2 font-sans font-light text-grey-dark">Hello, i'm from another the other
+                                    side!</p>
+                            </div>
+                            <div class="flex justify-center pb-3 text-grey-dark">
+                                <div class="text-center mr-3 border-r pr-3">
+                                    <h2>34</h2>
+                                    <span>Photos</span>
+                                </div>
+                                <div class="text-center">
+                                    <h2>42</h2>
+                                    <span>Friends</span>
+                                </div>
+                            </div>
+                        </div>
+                        <!--Top user 3-->
+                        <div class="rounded rounded-t-lg overflow-hidden shadow max-w-xs my-3">
+                            <img src="https://therichpost.com/wp-content/uploads/2021/04/flip-3.jpg" alt="" class="w-full"/>
+                            <div class="flex justify-center -mt-8">
+                                <img src="https://therichpost.com/wp-content/uploads/2021/03/avatar8.png" alt=""
+                                     class="responsvie-img rounded-full border-solid border-white border-2 -mt-3">
+                            </div>
+                            <div class="text-center px-3 pb-6 pt-2">
+                                <h3 class="text-black text-sm bold font-sans">Olivia Dunham</h3>
+                                <p class="mt-2 font-sans font-light text-grey-dark">Hello, i'm from another the other
+                                    side!</p>
+                            </div>
+                            <div class="flex justify-center pb-3 text-grey-dark">
+                                <div class="text-center mr-3 border-r pr-3">
+                                    <h2>34</h2>
+                                    <span>Photos</span>
+                                </div>
+                                <div class="text-center">
+                                    <h2>42</h2>
+                                    <span>Friends</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--/Onglets de profil-->
+                </div>
+            </main>
+            <!--/Principal-->
+        </div>
+    </div>
 </div>
--- resources/views/livewire/admin/v-users.blade.php
+++ resources/views/livewire/admin/v-users.blade.php
@@ -1,3 +1,43 @@
-<div>
-    {{-- If you look to others for fulfillment, you will never truly be fulfilled. --}}
-</div>
+                    <div class="flex flex-1 md:flex-row lg:flex-row p-1 mt-2 lg:mx-2 md:mx-2">
+                            <div class="rounded rounded-t-lg bg-white w-100 md:w-auto overflow-hidden shadow  my-3">
+                                <div class="border-b border-light-grey">
+                                    <div class="py-2 px-4 font-bold text-xl">Grille des utilisateurs</div>
+                                </div>
+                                
+                                <div class="grid grid-cols-4 gap-4 px-4 py-4 table-responsive">
+                                    <table class="table-auto">
+                                    <thead class="border-2 bg-red-700 text-white">
+                                        <tr>
+                                            <th>Song</th>
+                                            <th>Artist</th>
+                                            <th>Year</th>
+                                            <th>Modifier</th>
+                                            <th>Supprimer</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="border-2">
+                                        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
+                                        <td>Malcolm Lockyer</td>
+                                        <td>1961</td>
+                                        <th>
+                                            <button class="px-2 text-white bg-red-700">
+                                                Envoyer
+                                            </button>
+                                        </th>
+                                        </tr>
+                                        <tr class="border-2">
+                                        <td>Witchy Woman</td>
+                                        <td>The Eagles</td>
+                                        <td>1972</td>
+                                        </tr>
+                                        <tr class="border-2">
+                                        <td>Shining Star</td>
+                                        <td>Earth, Wind, and Fire</td>
+                                        <td>1975</td>
+                                        </tr>
+                                    </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                    </div>
